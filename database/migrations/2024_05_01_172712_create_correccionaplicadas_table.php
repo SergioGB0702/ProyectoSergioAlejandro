986c5003dba316194d48a12cbbf7880e347a9322
SergioGB0702 <sergioggbb02@gmail.com>
2024-05-01 20:13:23 +0200

    correccion nombre

--- database/migrations/2024_05_01_172712_create_correccionaplicadas_table.php
+++ database/migrations/2024_05_01_172712_create_correccionaplicadas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('conductaaplicadas', function (Blueprint $table) {
+        Schema::create('correccionaplicadas', function (Blueprint $table) {
             $table->id();
             $table->string("descripcion");
             $table->timestamps();
