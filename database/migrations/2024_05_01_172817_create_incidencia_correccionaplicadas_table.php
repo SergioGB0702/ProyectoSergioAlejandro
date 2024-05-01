af3c9300b52bc2c4046f4eedd109b8705e550bba
SergioGB0702 <sergioggbb02@gmail.com>
2024-05-01 20:41:09 +0200

    Correccion drop

--- database/migrations/2024_05_01_172817_create_incidencia_correccionaplicadas_table.php
+++ database/migrations/2024_05_01_172817_create_incidencia_correccionaplicadas_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('incidencia_conductasaplicadas');
+        Schema::dropIfExists('incidencia_correccionaplicadas');
     }
 };
